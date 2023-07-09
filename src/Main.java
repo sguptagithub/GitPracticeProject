440e0d677cfd4ed83f5ef8cca80edd2aad01fe09
sguptagithub <sunitagupta1@gmail.com>
2023-07-09 10:03:08 -0400

    GitPractice

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[]args){
+        solarSystemBasic Omnax = new solarSystemBasic();
+        Omnax.Moon ="2";
+        Omnax.Sun ="2";
+        Omnax.Star ="1";
+        Omnax.Planets ="4";
+
+
+    }
+}
